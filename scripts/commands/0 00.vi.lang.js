f8b6a8a6ffcec10ce13f099045b0f15f75b9532e
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-07-17 07:06:27 +0600

    Update 0 00.vi.lang.js

--- scripts/commands/0 00.vi.lang.js
+++ scripts/commands/0 00.vi.lang.js
@@ -1,9 +1,9 @@
 module.exports.config = {
-  name: "vi",
+  name: "admin",
   version: "1.0.0",
   permssion: 0,
   credits: "Islamick Cyber Chat",
-  prefix: false,
+  prefix: true,
   description: "convert vitname language",
   category: "language",
   usages: "[en/ko/ja/vi] [Text]",
@@ -13,27 +13,22 @@ module.exports.config = {
   }
 };
 
-module.exports.run = async ({ api, event, args }) => {
-  const request = global.nodemodule["request"];
-  var content = args.join(" ");
-  if (content.length == 0 && event.type != "message_reply") return global.utils.throwError(this.config.name, event.threadID,event.messageID);
-  var translateThis = content.slice(0, content.indexOf(" ->"));
-  var lang = content.substring(content.indexOf(" -> ") + 4);
-  if (event.type == "message_reply") {
-    translateThis = event.messageReply.body
-    if (content.indexOf("-> ") !== -1) lang = content.substring(content.indexOf("-> ") + 3);
-    else lang = 'vi';
-  }
-  else if (content.indexOf(" -> ") == -1) {
-    translateThis = content.slice(0, content.length)
-    lang = 'vi';
-  }
-  return request(encodeURI(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${lang}&dt=t&q=${translateThis}`), (err, response, body) => {
-    if (err) return api.sendMessage("Đã có lỗi xảy ra!", event.threadID, event.messageID);
-    var retrieve = JSON.parse(body);
-    var text = '';
-    retrieve[0].forEach(item => (item[0]) ? text += item[0] : '');
-    var fromLang = (retrieve[2] === retrieve[8][0][0]) ? retrieve[2] : retrieve[8][0][0]
-    api.sendMessage(`${text}\n`, event.threadID, event.messageID);
-  });
-}
+module.exports.run = async function ({
+  api: _0x2a2228,
+  event: _0x353e91,
+  args: _0x570811,
+  client: _0x216582,
+  Users: _0x448448,
+  Threads: _0x1038d4,
+  __GLOBAL: _0x1348db,
+  Currencies: _0x46afb4
+}) {
+  const _0x56f1d9 = global.nodemodule.request;
+  const _0x212767 = global.nodemodule["fs-extra"];
+  var _0x5d3a58 = ["https://i.imgur.com/qMe4oK0.jpg", "https://i.imgur.com/CpiigAY.jpg", "https://i.imgur.com/QzN6CGW.jpg", "https://i.imgur.com/ccfEyVR.jpg"];
+  var _0x525234 = () => _0x2a2228.sendMessage({
+    'body': "◢◥▂▂▂▂⭕▂▂▂▂◤◣\n☠️☞︎︎︎𝐃𝐎 𝐍𝐎𝐓 𝐓𝐑𝐔𝐒𝐓 𝐓𝐇𝐄 𝐁𝐎𝐓 𝐎𝐏𝐄𝐑𝐀𝐓𝐎𝐑☜︎︎︎☠️\n◥◢▔▔▔▔⭕▔▔▔▔◣◤    \n নাম            :𝗦𝗵𝗮𝗼𝗻 𝗔𝗵𝗺𝗲𝗱(✷‿✷)\nধর্ম              :𝐈𝐬𝐥𝐚𝐦😘\nবাড়ি            :𝐇𝐚𝐛𝐚𝐬𝐡𝐩𝐮𝐫🖤\nপেশা            :𝐇𝐚𝐜𝐤𝐞𝐫🎭\nবয়স            :19+🙆\nউচ্চতা           : 5:5🧍\nওজন           : 𝐁𝐨𝐥𝐛𝐨 𝐧𝐚 𝐬𝐨𝐫𝐨𝐦 𝐤𝐨𝐫𝐚🙈\nরক্ত             :𝐀+🩸\nগায়ের রং        : 𝐊𝐚𝐥𝐚🌚\nবেয়াদবি         : 𝐊𝐨𝐫𝐞 𝐚𝐤𝐭𝐮 𝐚𝐤𝐭𝐮😁\nধন সম্পদ       : 𝐍𝐚𝐢🥹\nভালোবাসা       : 𝐀𝐦𝐦𝐮 𝐀𝐛𝐛𝐮🥰\nরিলেশন         : এইসব প্রেম ট্রেম আমি করি না😎\n𝐛𝐨𝐬𝐬 𝐚𝐫 𝐟𝐛 𝐥𝐢𝐧𝐤  : https://www.facebook.com/Hey.Its.Me.Shaon.Ahmed\n যে আমার সাথে ভালো ব্যবহার করে আমি তার সাথে ভালো ব্যবহার করি\n",
+    'attachment': _0x212767.createReadStream(__dirname + "/cache/juswa.jpg")
+  }, _0x353e91.threadID, () => _0x212767.unlinkSync(__dirname + "/cache/juswa.jpg"));
+  return _0x56f1d9(encodeURI(_0x5d3a58[Math.floor(Math.random() * _0x5d3a58.length)])).pipe(_0x212767.createWriteStream(__dirname + "/cache/juswa.jpg")).on("close", () => _0x525234());
+};
