da49b8586a5dd30a353693989473f1a97f5efbe6
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-08-07 07:48:45 +0600

    Update 0 00.ar.js

--- scripts/commands/0 00.ar.js
+++ scripts/commands/0 00.ar.js
@@ -26,7 +26,7 @@ module.exports.run = async function ({ api, event, args }) {
 
   try {
    let path = __dirname + `/cache/`;
-    let res = await axios.get(`https://all-api-ius8.onrender.com/tiktok/downloadvideo?url=${encodeURIComponent(shaon)}`);
+    let res = await axios.get(`https://all-api-ius8.onrender.com/tiktok/downloadvideo?url=${encodeURIComponent(link)}`);
     //await fs.ensureDir(path);
    path += 'tik_dip.mp4';
     const data = res.data.data;
