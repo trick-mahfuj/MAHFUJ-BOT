52ae650b49892f206d9d8bba86b5c1cca1030246
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-08-07 09:57:36 +0600

    Update join.js

--- scripts/events/join.js
+++ scripts/events/join.js
@@ -2,16 +2,17 @@ module.exports.config = {
   name: "join",
   eventType: ['log:subscribe'],
   version: "1.0.0",
-  credits: "Mirai-Team", // FIXED BY YAN MAGLINTE
+  credits: "Mirai-Team", // FIXED BY YAN Nayan
   description: "GROUP UPDATE NOTIFICATION"
 };
+
 const fs = require('fs-extra');
 const { loadImage, createCanvas, registerFont } = require("canvas");
 const request = require('request');
-const { join } = require('path');
+//const { join } = require('path');
 const axios = require('axios');
 const jimp = require("jimp")
-const fontlink = 'https://drive.google.com/u/0/uc?id=1ZwFqYB-x6S9MjPfYm3t3SP1joohGl4iw&export=download'
+const fontlink = 'https://drive.google.com/u/0/uc?id=10XFWm9F6u2RKnuVIfwoEdlav2HhkAUIB&export=download'
 let PRFX = `${global.config.PREFIX}`;
 
 module.exports.circle = async (image) => {
@@ -25,9 +26,9 @@ let suffix;
 module.exports.run = async function({ api, event, Users }) {
   var fullYear = global.client.getTime("fullYear");
   var getHours = await global.client.getTime("hours");
-  var session = `${getHours < 3 ? "mid night" : getHours < 8 ? "Early morning" : getHours < 12 ? "noon" : getHours < 17 ? "afternoon" : getHours < 23 ? "evening" : "mid night"}`
+  var session = `${getHours < 3 ? "midnight" : getHours < 8 ? "Early morning" : getHours < 12 ? "noon" : getHours < 17 ? "afternoon" : getHours < 23 ? "evening" : "midnight"}`
   const moment = require("moment-timezone");
-  var thu = moment.tz('Asia/Dhaka').format('dddd');
+  var thu = moment.tz('Asia/dhaka').format('dddd');
   if (thu == 'Sunday') thu = 'Sunday'
   if (thu == 'Monday') thu = 'Monday'
   if (thu == 'Tuesday') thu = 'Tuesday'
@@ -35,21 +36,55 @@ module.exports.run = async function({ api, event, Users }) {
   if (thu == "Thursday") thu = 'Thursday'
   if (thu == 'Friday') thu = 'Friday'
   if (thu == 'Saturday') thu = 'Saturday'
-  const time = moment.tz("Asia/Dhaka").format("hh:mm:ss A - DD/MM/YYYY");
-  const hours = moment.tz("Asia/Dhaka").format("hh");
+  const time = moment.tz("Asia/dhaka").format("HH:mm:ss - DD/MM/YYYY");
+  const hours = moment.tz("Asia/dhaka").format("HH");
   const { commands } = global.client;
   const { threadID } = event;
   let threadInfo = await api.getThreadInfo(event.threadID);
   let threadName = threadInfo.threadName;
+  if (!event.logMessageData.addedParticipants || !Array.isArray(event.logMessageData.addedParticipants)) {
+    return;
+  }
+  if (event.logMessageData.addedParticipants && Array.isArray(event.logMessageData.addedParticipants) && event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
+    //api.changeNickname(`𝗕𝗢𝗧 ${(!global.config.BOTNAME) ? "Buddy" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
+
+    let gifUrl = 'https://i.postimg.cc/Rh0CGnSs/EMON.gif';
+let gifPath = __dirname + '/Emon/join/join.gif';
+
+axios.get(gifUrl, { responseType: 'arraybuffer' })
+.then(response => {
+    fs.writeFileSync(gifPath, response.data);
   if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
-    api.changeNickname(` ${(!global.config.BOTNAME) ? "Buddy" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
-    return api.sendMessage("", event.threadID, () => api.sendMessage({body:`✅ Group Connection in ${threadName} at ${session} Success....\n──────────────────\n→ Current Command: ${commands .size}\n→ Command sign: ${global.config.PREFIX}\n→ Version: ${global.config.version}\n→ Admin: <SHAON AHMED>\n→ Facebook:https://www.facebook.com/Hey.Its.Me.Shaon.Ahmed\n📌 Use ${PRFX}help to view command details\n⏰ Add bot at: ${time} 〈 ${thu} 〉`, attachment: fs.createReadStream(__dirname + "/cache/joinGif/randomgif/join.gif")}, threadID));
+    api.changeNickname(`[ ${global.config.PREFIX} ] • ➠${(!global.config.BOTNAME) ? "bot" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
+    return api.sendMessage("চলে এসেছি আমি শাওন বট তোমাদের মাঝে🤭!", event.threadID, () => api.sendMessage({ body: `${global.config.BOTNAME} CONNECTED«\n\nAssalamualaykum☘️
+<------------------------------>  
+BOT CONNECTED SUCCESFUL !!! 
+
+APPROVAL ALLOW IN THIS GROUP!!!
+<------------------------------>\n\nTO VIEW ANY COMMAND Use👉${global.config.PREFIX}help\nSee command👉 ${global.config.PREFIX}command
+\n\nEXAMPLE:👇\n${global.config.PREFIX}admin (information)\n${global.config.PREFIX}islam (video)\n${global.config.PREFIX}tik (link)\n${global.config.PREFIX}fbvideo (link)
+<------------------------------>
+AND FOR ANY COMPLAINTS OR CONTACT BOT OPERATOR 
+
+DEVELOPER : SHAON AHMED
+
+🟣Facebook Account Link: 
+
+https://www.facebook.com/100090879572769
+
+🔵WHATSAPP NUMBER: wa.me/+8801814587247
+
+🟢SUPPORT EMAIL: shaonproject25@gmail.com`, attachment: fs.createReadStream(gifPath)}, threadID));
+  }})
+.catch(error => {
+    console.error(error);
+});
   }
   else {
     try {
-      if (!fs.existsSync(__dirname + `/cache/font/Semi.ttf`)) {
+      if (!fs.existsSync(__dirname + `/Emon/font/Semi.ttf`)) {
         let getfont = (await axios.get(fontlink, { responseType: "arraybuffer" })).data;
-        fs.writeFileSync(__dirname + `/cache/font/Semi.ttf`, Buffer.from(getfont, "utf-8"));
+        fs.writeFileSync(__dirname + `/Emon/font/Semi.ttf`, Buffer.from(getfont, "utf-8"));
       };
       const { createReadStream, existsSync, mkdirSync, readdirSync } = global.nodemodule["fs-extra"];
       let { threadName, participantIDs } = await api.getThreadInfo(threadID);
@@ -65,15 +100,11 @@ module.exports.run = async function({ api, event, Users }) {
       }
       // console.log(event.logMessageData.addedParticipants)
       var id = [];
-      for (o = 0; o < event.logMessageData.addedParticipants.length; o++) {
-        let pathImg = __dirname + `/cache/join/${o}.png`;
-        let pathAva = __dirname + `/cache/join/avt.png`;
+      for (let o = 0; o < event.logMessageData.addedParticipants.length; o++) {
+        let pathImg = __dirname + `/Emon/join/${o}.png`;
+        let pathAva = __dirname + `/Emon/join/avt.png`;
         let avtAnime = (await axios.get(encodeURI(
-          `https://graph.facebook.com/${event.logMessageData.addedParticipants[o].userFbId}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`/*{
-          headers: {
-            cookie: 'xs=13%3AlajUELLXiJWSGA%3A2%3A1670218018%3A-1%3A6326;c_user=100026039134645;fr=04yhWc9aZ2jCK6WYB.AWX6H8d2OYiFMQa_tmHEcMP9bNY.BjjYEe.-o.AAA.0.0.BjjYEi.AWW2404AO5I;sb=HoGNYx-MLHOu0FOMeC8kqttW;datr=HoGNY-xBBNLJjRghcnhN1hWA;'
-          }
-        }*/), { responseType: "arraybuffer" })).data;
+          `https://graph.facebook.com/${event.logMessageData.addedParticipants[o].userFbId}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`), { responseType: "arraybuffer" })).data;
         var ok = [
           'https://i.imgur.com/dDSh0wc.jpeg',
           'https://i.imgur.com/UucSRWJ.jpeg',
@@ -87,7 +118,7 @@ module.exports.run = async function({ api, event, Users }) {
         var avatar = await this.circle(pathAva);
         let baseImage = await loadImage(pathImg);
         let baseAva = await loadImage(avatar);
-        registerFont(__dirname + `/cache/font/Semi.ttf`, {
+        registerFont(__dirname + `/Emon/font/Semi.ttf`, {
           family: "Semi"
         });
         let canvas = createCanvas(1902, 1082);
@@ -103,36 +134,36 @@ module.exports.run = async function({ api, event, Users }) {
         ctx.save();
         ctx.font = `75px Semi`;
         ctx.fillText(`Welcome to ${threadName}`, canvas.width / 2 - 15, canvas.height / 2 + 235)
-const number = participantIDs.length - o;
+        const number = participantIDs.length - o;
 
-if (number === 11 || number === 12 || number === 13) {
-  suffix = "th";
-} else {
-  const lastDigit = number % 10;
-  switch (lastDigit) {
-    case 1:
-      suffix = "st";
-      break;
-    case 2:
-      suffix = "nd";
-      break;
-    case 3:
-      suffix = "rd";
-      break;
-    default:
-      suffix = "th";
-      break;
-  }
-}
+        if (number === 11 || number === 12 || number === 13) {
+          suffix = "th";
+        } else {
+          const lastDigit = number % 10;
+          switch (lastDigit) {
+            case 1:
+              suffix = "st";
+              break;
+            case 2:
+              suffix = "nd";
+              break;
+            case 3:
+              suffix = "rd";
+              break;
+            default:
+              suffix = "th";
+              break;
+          }
+        }
 
-ctx.fillText(`You are the ${number}${suffix} member of this group`, canvas.width / 2 - 15, canvas.height / 2 + 350);
+        ctx.fillText(`You are the ${number}${suffix} member of this group`, canvas.width / 2 - 15, canvas.height / 2 + 350);
         ctx.restore();
         const imageBuffer = canvas.toBuffer();
         fs.writeFileSync(pathImg, imageBuffer);
-        abx.push(fs.createReadStream(__dirname + `/cache/join/${o}.png`))
+        abx.push(fs.createReadStream(__dirname + `/Emon/join/${o}.png`))
       }
       memLength.sort((a, b) => a - b);
-      (typeof threadData.customJoin == "undefined") ? msg = `🌟 Welcome new member {name} to the group {threadName}\n→ URL Profile:\nhttps://www.facebook.com/profile.php?id={iduser}\n→ {type} are the group's {soThanhVien}${suffix} member\n→ Added to the group by: {author}\n→ Added by facebook link: https://www.facebook.com/profile.php?id={uidAuthor}\n─ ─────────────────\n[ {time} - {thu} ]` : msg = threadData.customJoin;
+      (typeof threadData.customJoin == "undefined") ? msg = `Hello {name}\nWelcome to {threadName}\nyou're the {soThanhVien}th member on this group please enjoy"\n─────────────────\n[ {time} - {thu} ]` : msg = threadData.customJoin;
       var nameAuthor = await Users.getNameUser(event.author)
       msg = msg
         .replace(/\{iduser}/g, iduser.join(', '))
@@ -149,8 +180,8 @@ ctx.fillText(`You are the ${number}${suffix} member of this group`, canvas.width
       var formPush = { body: msg, attachment: abx, mentions }
       api.sendMessage(formPush, threadID);
       for (let ii = 0; ii < parseInt(id.length); ii++) {
-        fs.unlinkSync(__dirname + `/cache/join/${ii}.png`)
+        fs.unlinkSync(__dirname + `/Emon/join/${ii}.png`)
       }
     } catch (e) { return console.log(e) };
   }
-}
+	      }
