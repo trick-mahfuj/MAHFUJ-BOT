253af15f0d30890581312cb8fef0e530ce526962
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-08-08 11:49:47 +0600

    Update 4k.js

--- scripts/commands/4k.js
+++ scripts/commands/4k.js
@@ -52,7 +52,6 @@ module.exports.handleReply = async ({
 };
 
 async function linkanh(choice) {
-  const axios = require("axios");
   const apis = await axios.get('https://all-api-ius8.onrender.com');
   const n = apis.data.data
   const options = {
