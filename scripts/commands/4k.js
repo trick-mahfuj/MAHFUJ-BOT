1ce3af650f895a44f67fa87f676ecd73005f17a7
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-08-08 12:03:39 +0600

    Update 4k.js

--- scripts/commands/4k.js
+++ scripts/commands/4k.js
@@ -1,29 +1,13 @@
 module.exports.config = {
-
-    name: "album",
-
-    version: "1.0.0",
-
-    permission: 0,
-
-    credits: "Nayan",
-
-    description: "Photo Convert HD",
-
-    prefix: 'awto',
-
-    category: "user",
-
-    usages: "hd [Reply photo]",
-
-    cooldowns: 10,
-
-    dependencies: {
-
-       'nayan-server': ''
-
-    }
-
+  name: "album",
+  version: "0.0.3",
+  permission: 0,
+  prefix: 'awto',
+  credits: "Nayan",
+  description: "Random video",
+  category: "user",
+  usages: "",
+  cooldowns: 5,
 };
 
 module.exports.run = async function({
@@ -31,7 +15,7 @@ module.exports.run = async function({
   api: a,
   args: n
 }) {
-  if (!n[0]) return a.sendMessage("╭──────•𝚂𝙷𝙰𝙾𝙽 𝙿𝚁𝙾𝙹𝙴𝙲𝚃•──────╮\n\n☢━━━━━━💛𝙰𝙻𝙱𝚄𝙼 𝚅𝙸𝙳𝙴𝙾💛━━━━━━☢ \n!\n!➤1 𝙸𝚂𝙻𝙰𝙼 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤2 𝙰𝙽𝙸𝙼𝙴 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤3 𝚂𝙷𝙰𝙸𝚁𝙸 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤4 𝚂𝙷𝙾𝚁𝚃 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤5 𝚂𝙰𝙳𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤6 𝚂𝚃𝙰𝚃𝚄𝚂 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤7 𝙵𝙾𝙾𝚃𝙱𝙰𝙻𝙻 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤8 𝙵𝚄𝙽𝙽𝚈 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤9 𝙻𝙾𝚅𝙴 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤10 𝙲𝙿𝙻 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤11 𝙱𝙰𝙱𝚈 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤12 𝙵𝚁𝙴𝙴 𝙵𝙸𝚁𝙴 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤13 𝙻𝙾𝙵𝙸 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤14 𝙷𝙰𝙿𝙿𝚈 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤15 𝙷𝚄𝙼𝙰𝙸𝚈𝚄𝙽 𝚂𝙸𝚁 𝚅𝙸𝙳𝙴𝙾◄┈╯\n☢━━━━━━💚𝙷𝙾𝚃 𝚅𝙸𝙳𝙴𝙾━━━━━━☢\n!➤16 𝚂𝙴𝚇 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤17 𝙷𝙾𝚁𝙽𝚈 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤18 𝙸𝚃𝙴𝙼 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤19 𝙷𝙾𝚃 𝚅𝙸𝙳𝙴𝙾◄┈╯\n!\n!➤20 𝙲𝙰𝙿𝙲𝚄𝚃 𝚅𝙸𝙳𝙴𝙾◄┈╯\n━━━━━━━━━━━━━━━━━━━━━\n𝙾𝚆𝙽𝙴𝚁:𝚂𝙷𝙰𝙾𝙽 𝙰𝙷𝙼𝙴𝙳\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n𝙰 𝙿 𝙸//𝚂𝙷𝙰𝙾𝙽\n━━━━━━━━━━━━━━━━━━━━━\n╰──────•𝚂𝙷𝙰𝙾𝙽 𝙿𝚁𝙾𝙹𝙴𝙲𝚃•──────╯\n\nTell me how many video numbers you want to see by replaying this message", e.threadID, ((a, n) => {
+  if (!n[0]) return a.sendMessage("====「 𝐕𝐈𝐃𝐄𝐎 」====\n━━━━━━━━━━━━━\n𝟙. 𝐋𝐎𝐕𝐄 𝐕𝐈𝐃𝐄𝐎 💞 \n𝟚. 𝐂𝐎𝐔𝐏𝐋𝐄 𝐕𝐈𝐃𝐄𝐎 💕\n𝟛. 𝐒𝐇𝐎𝐑𝐓 𝐕𝐈𝐃𝐄𝐎 📽\n𝟜. 𝐒𝐀𝐃 𝐕da𝐄𝐎 😔\n𝟝. 𝐒𝐓𝐀𝐓𝐔𝐒 𝐕𝐈𝐃𝐄𝐎 📝\n𝟞. 𝐒𝐇𝐀𝐈𝐑𝐈\n𝟟. 𝐁𝐀𝐁𝐘 𝐕𝐈𝐃𝐄𝐎 😻\n𝟠. 𝐀𝐍𝐈𝐌𝐄 𝐕𝐈𝐃𝐄𝐎 \n𝟡. 𝐇𝐔𝐌𝐀𝐈𝐘𝐔𝐍 𝐅𝐎𝐑𝐈𝐃 𝐒𝐈𝐑 ❄\n𝟙𝟘. 𝐈𝐒𝐋𝐀𝐌𝐈𝐊 𝐕𝐈𝐃𝐄𝐎 🤲\n\n===「 𝟏𝟖+ 𝐕𝐈𝐃𝐄𝐎 」===\n━━━━━━━━━━━━━\n𝟙𝟙. 𝐇𝐎𝐑𝐍𝐘 𝐕𝐈𝐃𝐄𝐎 🥵\n𝟙𝟚. 𝐇𝐎𝐓 🔞\n𝟙𝟛. 𝐈𝐓𝐄𝐌\n\nTell me how many video numbers you want to see by replying to this message", e.threadID, ((a, n) => {
     global.client.handleReply.push({
       name: this.config.name,
       messageID: n.messageID,
@@ -39,7 +23,9 @@ module.exports.run = async function({
       type: "create"
     })
   }), e.messageID)
-  }, module.exports.handleReply = async ({
+}; 
+
+module.exports.handleReply = async ({
   api: e,
   event: a,
   client: n,
@@ -47,69 +33,42 @@ module.exports.run = async function({
   Currencies: s,
   Users: i,
   Threads: o
-  }) => {
-  var { p, h } = linkanh();
-
+}) => {
+  var { p, h } = await linkanh(a.body);
+  const axios = require("axios");
   if ("create" === t.type) {
-    const n = (await p.get(h)).data.data;
-    const shaon = (await p.get(h)).data.shaon;
-    const ls = (await p.get(h)).data.count;
-    let t = (await p.get(n, {
+    const response = await p.get(h);
+    const cap = response.data.shaon;
+    const cn = response.data.count;
+    let nayan = (await p.get(data, {
       responseType: "stream"
     })).data;
     return e.sendMessage({
-      body: `🟡${shaon}\n𝚃𝙾𝚃𝙰𝙻 𝚅𝙸𝙳𝙴𝙾:${ls}\n𝙰 𝙿 𝙸  𝚂 𝙷 𝙰 𝙾 𝙽📛`,
-      attachment: t
+      body: `${cap}` + `\n\n¤《𝐓𝐎𝐓𝐀𝐋 𝐕𝐈𝐃𝐄𝐎: ${cn}》¤`,
+      attachment: nayan
     }, a.threadID, a.messageID)
   }
-
-  function linkanh() {
-        const p = require("axios");
-        if ("1" == a.body)
-            var h = "https://all-api-ius8.onrender.com/video/islam";
-        else if ("2" == a.body)
-         var   h = "https://all-api-ius8.onrender.com/video/anime";
-        else if ("3" == a.body)
-         var   h = "https://all-api-ius8.onrender.com/video/shairi";
-        else if ("4" == a.body)
-          var  h = "https://all-api-ius8.onrender.com/video/short";
-        else if ("5" == a.body)
-          var  h = "https://all-api-ius8.onrender.com/video/sad";
-        else if ("6" == a.body)
-          var  h = "https://all-api-ius8.onrender.com/video/status";
-        else if ("7" == a.body)
-          var  h = "https://all-api-ius8.onrender.com/video/football";
-        else if ("8" == a.body)
-          var  h = "https://all-api-ius8.onrender.com/video/funny";
-        else if ("9" == a.body)
-         var   h = "https://all-api-ius8.onrender.com/video/love";
-        else if ("10" == a.body)
-         var  h = "https://all-api-ius8.onrender.com/video/cpl";
-          else if ("11" == a.body)
-           var  h = "https://all-api-ius8.onrender.com/video/baby";
-          else if ("12" == a.body)
-          var  h = "https://all-api-ius8.onrender.com/video/kosto";
-         else if ("13" == a.body)
-         var  h = "https://all-api-ius8.onrender.com/video/lofi";
-         else if ("14" == a.body)
-         var  h = "https://all-api-ius8.onrender.com/video/happy";
-         else if ("15" == a.body)
-         var  h = "https://all-api-ius8.onrender.com/video/humaiyun";
-           else if ("16" == a.body)
-         var  h =
-"https://all-api-ius8.onrender.com/video/sex";
-    else if ("17" == a.body)
-         var  h =
-"https://all-api-ius8.onrender.com/video/horny";
-    else if ("18" == a.body)
-         var  h =
-"https://all-api-ius8.onrender.com/video/item";
-    else if ("19" == a.body)
-         var  h =
-"https://all-api-ius8.onrender.com/video/hot";
-    else if ("20" == a.body)
-         var  h =
-"https://all-api-ius8.onrender.com/video/capcut";
-        return { p, h };
-    }
 };
+
+async function linkanh(choice) {
+  const axios = require("axios");
+  const apis = await axios.get('https://all-api-ius8.onrender.com');
+  const n = apis.data.data
+  const options = {
+    "1": "/video/love",
+    "2": "/video/cpl",
+    "3": "/video/shortvideo",
+    "4": "/video/sadvideo",
+    "5": "/video/status",
+    "6": "/video/shairi",
+    "7": "/video/baby",
+    "8": "/video/anime",
+    "9": "/video/humaiyun",
+    "10": "/video/islam",
+    "11": "/video/horny",
+    "12": "/video/hot",
+    "13": "/video/item"
+  };
+  const h = `${n}${options[choice]}`;
+  return { p: axios, h };
+}
