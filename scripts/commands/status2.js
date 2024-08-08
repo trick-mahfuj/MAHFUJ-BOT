3d1e8ce107392e0665766170163f61584fa2f458
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-08-08 15:53:03 +0600

    Update status2.js

--- scripts/commands/status2.js
+++ scripts/commands/status2.js
@@ -1,9 +1,9 @@
 module.exports.config = {
-  name: "status",
+  name: "/",
   version: "1.0.0", 
   permssion: 0,
   credits: "Islamick Cyber Chat", 
-  prefix: 'true',
+  prefix: 'false',
   description: " stutus video",
   category: "system video", 
   usages: "", 
