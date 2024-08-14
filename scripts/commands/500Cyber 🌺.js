9adda0974a7d5e6644e605a082169680b9115a6b
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-08-14 14:00:35 +0600

    Update 500Cyber 🌺.js

--- scripts/commands/500Cyber 🌺.js
+++ scripts/commands/500Cyber 🌺.js
@@ -1,14 +1,5 @@
-const axios = require("axios");
-const fs = require("fs");
-const request = require("request");
-
-const link = [
-  "https://i.imgur.com/9H68RaB.mp4",
-
-];
-
 module.exports.config = {
-  name: "🌺",
+  name: "dp9",
   version: "1.0.0",
   permssion: 0,
   credits: "Islamick Cyber Chat",
@@ -24,47 +15,68 @@ module.exports.config = {
   }
 };
 
-module.exports.handleEvent = async ({ api, event, Threads }) => {
-  const content = event.body ? event.body : '';
-    const body = content.toLowerCase();
-  if (body.startsWith("🌺")) {
-    const rahad = [
-      "•┄┅════❁🌺❁════┅┄•\n      -আল্লাহু আল্লাহ-!!✨🧡\n•┄┅════❁🌺❁════┅┄•",
-      "•┄┅════❁🌺❁════┅┄•\n       -আল্লাহু আল্লাহ-!!✨🧡\n•┄┅════❁🌺❁════┅┄•"
+module.exports.onLoad = async() => {
+    const { resolve } = global.nodemodule["path"];
+    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+    const { downloadFile } = global.utils;
+    const dirMaterial = __dirname + `/cache/`;
+    const path = resolve(__dirname, 'cache', 'lovev1.png');
+    if (!existsSync(dirMaterial + "")) mkdirSync(dirMaterial, { recursive: true });
+    if (!existsSync(path)) await downloadFile("https://drive.google.com/uc?id=1gf9_9b6wGWUvsXVIwSBIwxDzG64BjT86", path);
 
-    ];
-    const rahad2 = rahad[Math.floor(Math.random() * rahad.length)];
+}
 
-    const callback = () => api.sendMessage({
-      body: `${rahad2}`,
-      attachment: fs.createReadStream(__dirname + "/cache/2024.mp4")
-    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/2024.mp4"), event.messageID);
+async function makeImage({ one, two }) {
+    const fs = global.nodemodule["fs-extra"];
+    const path = global.nodemodule["path"];
+    const axios = global.nodemodule["axios"]; 
+    const jimp = global.nodemodule["jimp"];
+    const __root = path.resolve(__dirname, "cache");
 
-    const requestStream = request(encodeURI(link[Math.floor(Math.random() * link.length)]));
-    requestStream.pipe(fs.createWriteStream(__dirname + "/cache/2024.mp4")).on("close", () => callback());
-    return requestStream;
-  }
-};
+    let hon_img = await jimp.read(__root + "/lovev1.png");
+    let pathImg = __root + `/lovev1_${one}_${two}.png`;
+    let avatarOne = __root + `/avt_${one}.png`;
+    let avatarTwo = __root + `/avt_${two}.png`;
 
-module.exports.languages = {
-  "vi": {
-    "on": "Dùng sai cách rồi lêu lêu",
-    "off": "sv ngu, đã bão dùng sai cách",
-    "successText": `🧠`,
-  },
-  "en": {
-    "on": "on",
-    "off": "off",
-    "successText": "success!",
-  }
-};
+    let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
+    fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
 
-module.exports.run = async ({ api, event, Threads, getText }) => {
-  const { threadID, messageID } = event;
-  let data = (await Threads.getData(threadID)).data;
-  if (typeof data["🌺"] === "undefined" || data["🌺"]) data["🌺"] = false;
-  else data["🌺"] = true;
-  await Threads.setData(threadID, { data });
-  global.data.threadData.set(threadID, data);
-  api.sendMessage(`${(data["🌺"]) ? getText("off") : getText("on")} ${getText("successText")}`, threadID, messageID);
-}; 
+    let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
+    fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
+
+    let circleOne = await jimp.read(await circle(avatarOne));
+    let circleTwo = await jimp.read(await circle(avatarTwo));
+    hon_img.resize(1280, 695).composite(circleOne.resize(395, 395), 760, 135
+ ).composite(circleTwo.resize(395, 395), 155, 135);
+
+    let raw = await hon_img.getBufferAsync("image/png");
+
+    fs.writeFileSync(pathImg, raw);
+    fs.unlinkSync(avatarOne);
+    fs.unlinkSync(avatarTwo);
+
+    return pathImg;
+}
+async function circle(image) {
+    const jimp = require("jimp");
+    image = await jimp.read(image);
+    image.circle();
+    return await image.getBufferAsync("image/png");
+}
+
+module.exports.run = async function ({ event, api, args }) {
+    const fs = global.nodemodule["fs-extra"];
+    const { threadID, messageID, senderID } = event;
+    var mention = Object.keys(event.mentions)[0]
+    let tag = event.mentions[mention].replace("@", "");
+    if (!mention) return api.sendMessage("আপনার ভালোবাসার মানুষ টি কে টেগ করুন-!!💏🙈", threadID, messageID);
+    else {
+        var one = senderID, two = mention;
+        return makeImage({ one, two }).then(path => api.sendMessage({ body: "︵🦋🤍🍒\n\n___ღ༎-মন থেকে ভালোবেসে দেখো.!😻🥰\n\n"  +  tag + '\n\n___নির্দিষ্ট মানুষ ছাড়া আর অন্য কাউকে ভালো লাগবে না.!🖤😘\n\n ︵🦋🤍🍒',
+            mentions: [{
+          tag: tag,
+          id: mention
+        }],
+     attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+    }
+                    }
