763de225971c91c3e286b5d1af63d7b2ac53249a
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-08-22 21:01:04 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -23,36 +23,26 @@ const time = process.uptime(),
     minutes = Math.floor((time % (60 * 60)) / 60),
     seconds = Math.floor(time % 60);
 const moment = require("moment-timezone");
-var juswa = moment.tz("Asia/Manila").format("『D/MM/YYYY』 【hh:mm:ss】");
-var link = ["https://i.postimg.cc/DZ119SnX/Picsart-23-09-21-21-35-06-549.jpg", 
-
-            "https://i.postimg.cc/m2WmzTZQ/IMG-20230816-121917.jpg", 
-
-            "https://i.postimg.cc/zvsrftbD/Picsart-23-06-13-22-53-45-323.jpg",
-
-            "https://i.postimg.cc/pXJLXjYf/received-251496360719634.jpg"];
+var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
+var link = ["https://i.imgur.com/qMe4oK0.jpg", "https://i.imgur.com/CpiigAY.jpg", "https://i.imgur.com/QzN6CGW.jpg", "https://i.imgur.com/ccfEyVR.jpg"];
 
 var callback = () => api.sendMessage({body:`•—»✨𝐀𝐝𝐦𝐢𝐧 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧✨🌺
 •┄┅═════❁🌺❁═════┅┄•
 
- 𝐁𝐨𝐭 𝐍𝐚𝐦𝐞 : 𝐈𝐬𝐥𝐚𝐦𝐢𝐜𝐤 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭
+ 𝐁𝐨𝐭 𝐍𝐚𝐦𝐞 : 𝐒𝐡𝐚𝐨𝐧 𝐏𝐫𝐨𝐣𝐞𝐜𝐭
 
-𝐁𝐨𝐭 𝐀𝐝𝐦𝐢𝐧 : ছো্ঁট্টো্ঁ_ন্ঁবা্ঁব্ঁ500
+𝐁𝐨𝐭 𝐀𝐝𝐦𝐢𝐧 : শাওন আহমেদ
 
-𝐁𝐨𝐭 𝐎𝐰𝐧𝐞𝐫 : 𝐌𝐝 𝐉𝐚𝐡𝐢𝐝 𝐇𝐚𝐬𝐚𝐧 𝐑𝐚𝐣𝐢𝐛
+𝐁𝐨𝐭 𝐎𝐰𝐧𝐞𝐫 : 𝐒𝐡𝐚𝐨𝐧 𝐀𝐡𝐦𝐞𝐝
 
 •┄┅══❁CONCATET❁══┅┄• 
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐈𝐝: https://www.facebook.com/profile.php?id=100029115600589&mibextid=ZbWKwL
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐈𝐝: https://www.facebook.com/Hey.Its.Me.Shaon.Ahmed
 
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐈𝐝: https://www.facebook.com/jahidhasanrajib500
+𝐖𝐏 𝐍𝐮𝐦𝐛𝐞𝐫 : 01814587247
 
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐏𝐚𝐠𝐞 : https://www.facebook.com/profile.php?id=100081939442749
+𝐒𝐞𝐚𝐫𝐜𝐡 𝐆𝐨𝐨𝐠𝐥𝐞 : শাওন আহমেদ
 
-𝐖𝐏 𝐍𝐮𝐦𝐛𝐞𝐫 : 01859551262
-
-𝐒𝐞𝐚𝐫𝐜𝐡 𝐆𝐨𝐨𝐠𝐥𝐞 : ছো্ঁট্টো্ঁ_ন্ঁবা্ঁব্ঁ500
-
-𝐖𝐚𝐛 𝐒𝐢𝐭𝐞 𝐋𝐢𝐧𝐤 : https://linktr.ee/muslimhakcerbd
+𝐖𝐚𝐛 𝐒𝐢𝐭𝐞 𝐋𝐢𝐧𝐤 : https://linktr.ee/shaonahmed
 
 •┄┅═════❁🌺❁═════┅┄•\n🌺✨𝐎𝐭𝐡𝐞𝐫𝐬 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧✨🌺\n •┄┅═════❁🌺❁═════┅┄•
 
@@ -62,7 +52,7 @@ TYPE /admin
 
 𝐁𝐨𝐭 𝐏𝐫𝐞𝐟𝐢𝐱 : ${global.config.PREFIX}
 
-𝐁𝐨𝐭 𝐎𝐰𝐧𝐞𝐫 : 𝐌𝐝 𝐉𝐚𝐡𝐢𝐝 𝐇𝐚𝐬𝐚𝐧 𝐑𝐚𝐣𝐢𝐛
+𝐁𝐨𝐭 𝐎𝐰𝐧𝐞𝐫 : 𝐒𝐡𝐚𝐨𝐧 𝐀𝐡𝐦𝐞𝐝
 
 •—»✨ 𝐔𝐩𝐭𝐢𝐦𝐞
 
