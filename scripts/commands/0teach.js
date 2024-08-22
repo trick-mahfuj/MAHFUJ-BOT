6df1e7a6876eb025099a423a74442f4047185514
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-08-22 17:54:26 +0600

    Update 0teach.js

--- scripts/commands/0teach.js
+++ scripts/commands/0teach.js
@@ -2,42 +2,41 @@ const axios = require('axios');
 const jimp = require("jimp");
 const fs = require("fs");
 
-module.exports.config = {
+module.exports.config = { 
   name: "teach",
-  version: "11.9.7",
-  permssion: 0,
+  version: "0.0.2",
+  permission: 0,
+  prefix: 'awto',
   credits: "Shaon Ahmed",
-  prefix: false,
-  description: "sim",
-  category: "sim add",
-  usages: "teach",
-  cooldowns: 30,
+  description: "Teach sim",
+  category: "admin",
+  usages: "hi = hello",
+    cooldowns: 5,
 };
 
   module.exports.run = async function({ api, event, args, Users, Threads, Currencies}) {
     const uid = event.senderID;
     const info = args.join(" ");
-    var n = global.shaon_api
+    const apis = await axios.get('https://raw.githubusercontent.com/shaonproject/Shaon/main/api.json')
+  const teach = apis.data.sim
     var id = Object.keys(event.mentions)[0] || event.senderID;
   var nam = await Users.getNameUser(id);
   var ThreadInfo = await api.getThreadInfo(event.threadID);
     if (!info) {
-      return api.sendMessage(`Please enter in the format:\n${global.config.PREFIX}teach hi - hello`, event.threadID);
+      return api.sendMessage(`Please enter in the format:\n${global.config.PREFIX}teach hi = hello`, event.threadID);
     } else {
-      const msg = info.split("-");
-      const ID = msg[0].trim();
-      const ask = msg[1].trim();
-      const ans = msg[2].trim();
+      const msg = info.split("=");
+      const ask = msg[0].trim();
+      const ans = msg[1].trim();
 
 
-      const img = `https://all-api-ius8.onrender.com/sim?type=teach&ask=${ask}&ans=${ans}`
-
+      const img = `${teach}/sim?type=teach&ask=${ask}&ans=${ans}`
       try {
         const response = await axios.get(img);
 
 
                 api.sendMessage({ 
-          body: `📝Your Data Added To Database Successfully\nTotal Teach=${ID}\n1️⃣ASK: ${ask}\n2️⃣ANS: ${ans}`
+          body: `📝Your Data Added To Database Successfully\n1️⃣ASK: ${ask}\n2️⃣ANS: ${ans}`
                         }, event.threadID);
                       } catch (error) {
                         console.error(error);
